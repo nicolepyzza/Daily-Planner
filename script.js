c89302a97604b748ff444791f54026632b0edca0
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-03-25 12:48:34 -0400

    added text boxes, time

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
 var date;
-var time = moment().format("H");
-var currentDiv;
+var time;
+var currentTime = moment().format("H");
 
 $(window).on("load", function(){
-   
     setInterval('clockUpdate()', 1000);
-
-   for (i=0; i<=23; i++) {
-        currentDiv = i;
-        if (time == i) {
-            $('#' + currentDiv).addClass("present");
-            $('#' + currentDiv).children('div').children('div').children("textarea").addClass("present");
-        } else if (time > i) {
-            $('#' + currentDiv).addClass("past");
-            $('#' + currentDiv).children('div').children('div').children("textarea").addClass("past");
-        } else if (time < i) {
-            $('#' + currentDiv).addClass("future");
-            $('#' + currentDiv).children('div').children('div').children("textarea").addClass("future"); 
-        }
-    }
 });
 
 function clockUpdate() {
     date = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").html(date);
 }
-
