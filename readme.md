baf1db2c5f7123915fd0a7c8f3e900730c167b2b
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-03-26 10:59:06 -0400

    fixed loop, added readme

---
+++ readme.md
@@ -0,0 +1,33 @@
+## Acceptance Criteria
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## My Daily Planner
+ 
+```
+On page load, my daily planner lists the date and time, which refreshes every second
+It displays the hours 9 through 6 (a more accurate work day) and allows the user
+to input text for each hour and save into local storage
+Once the page reloads, it gets any information that was previously saved and displays it on the screen 
+```
+
+## GitHub Pages
+
+```
+https://nicoleobom.github.io/Week05/
+```
+
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Set appropriate variables
 var realTime;
-var currentHour = moment().format("H");
+var currentHour = moment().format('H');
 var array = [];
 var retrievedAppt;
 var showAppt;
@@ -7,11 +8,15 @@ var appt;
 var timeID;
 var text;
 var timeIndex;
+var currentDiv;
 
+// On load function
 $(window).on("load", function(){
+    // Call clockUpdate function and refresh every second
     setInterval('clockUpdate()', 1000);
-    //check local storage for textarea's value
 
+    // Function that will grab any previously saved data in local storage and display
+    // it on screen in the appropriate textareas.
     function getApptInfo(){
         retrievedAppt = JSON.parse(localStorage.getItem('appts'));
         if (retrievedAppt !== null) {
@@ -27,15 +32,31 @@ $(window).on("load", function(){
         }
     }
 
+    // Call the function to grab data in local storage upon page load
     getApptInfo();
 
 });
 
+// For each div, compare div id to the current hour and assign the appropriate CSS class
+for (i=0; i<23; i++) {
+    currentDiv = i;
+    if (currentHour == i) {
+        $('#' + currentDiv).addClass('present');
+    } else if (currentHour > i) {
+        $('#' + currentDiv).addClass('past');
+    } else {
+        $('#' + currentDiv).addClass('future');
+    }
+}
+
+// Using moment.js, add a clock/counter and display at the top of the page
 function clockUpdate() {
     realTime = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").html(realTime);
 }
 
+// On click for each save button, save the data of each text area AND
+// the id of the textarea div to local storage as an array.
 $(".saveBtn").on("click", function(){
     //get textarea's value
     text = $(this).parent('div').children('div').children('textarea').val();
@@ -44,7 +65,6 @@ $(".saveBtn").on("click", function(){
         time: timeID,
         details: text
     }
-    //put it into local storage
     array = JSON.parse(localStorage.getItem('appts'));
     if (array === null) {
         localStorage.setItem('appts', JSON.stringify([{ time: timeID, details: text }]));
